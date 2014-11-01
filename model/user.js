3f63afc2e9ecd60784a6e5ca97674251e134129c
Rachel Wang <rswang@mit.edu>
2014-11-01 13:10:28 -0400

    Add queue stuff

--- model/user.js
+++ model/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var distance = require('gps-distance');
 
 var userSchema = mongoose.Schema({
     givenName: {type: String},
@@ -12,4 +13,46 @@ var userSchema = mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('User', userSchema);
+var User = mongoose.model('User', userSchema);
+
+userSchema.method('addToQueue', function(latitude, longitude, callback) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.inQueue = true;
+    var matchedUser = this.match(function(err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(user);
+    });
+});
+
+userSchema.method('removeFromQueue', function() {
+    this.latitude = null;
+    this.longitude = null;
+    this.inQueue = false;
+});
+
+userSchema.method('match', function(callback) {
+    User.find({inQuene: true}).exec(function(err, users) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        for (var i = 0; i < users.length; i++) {
+            var user = users[i];
+            // kilometers
+            var maxDistance = 1.0;
+            if (distance(this.latitude, this.longitude, user.latitude, user.longitude) < maxDistance) {
+                this.removeFromQueue();
+                user.removeFromQueue();
+                callback(null, user);
+                return;
+            }
+        };
+        callback(null);
+    });
+});
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,14 @@ router.get('/hug', function(req, res) {
   res.render('hug');
 });
 
+router.post('/hug', function(req, res) {
+    req.user.addToQueue(req.body.latitude, req.body.longitude, function(user) {
+        if (user) {
+            res.status(200).send("We matched you!!!!");
+        } else {
+            res.status(200).send("Sorry, we're still matching you!");
+        }
+    });
+});
+
 module.exports = router;
